fe19b2219bffd311838b1a870fc99815c7576dd1
Takashi Yoshida <takashi@takashiyoshida.org>
2014-10-18 16:33:10 +0800

    Fix spacing issue

--- spec/classes/textexpander_spec.rb
+++ spec/classes/textexpander_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'textexpander' do
   it do
     should contain_package('TextExpander').with({
-      :ensure => 'installed',
+      :ensure   => 'installed',
       :provider => 'compressed_app'
     })
   end
